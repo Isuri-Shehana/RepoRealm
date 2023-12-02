825ed90a1d2a8736ca1f09f55dd358460c7d4e49
Isuri-Shehana <isurishehana2000@gmail.com>
2023-12-02 16:47:17 +0530

    Alignment feature is added

--- newscript.js
+++ newscript.js
@@ -15,3 +15,25 @@ function textColorPicker(){
     document.execCommand('foreColor',false,color_pick);
     document.getElementById('color-rect').style.backgroundColor = color_pick;
 }
+
+var left_Align = Text_alignment('left');
+var Center = Text_alignment('center');
+var right_Align = Text_alignment('right');
+var Justify= Text_alignment('justify');
+
+document.querySelectorAll('.left').forEach(function (element) {
+    element.addEventListener('click', left_Align);
+});
+
+document.querySelectorAll('.center').forEach(function (element) {
+    element.addEventListener('click',Center);
+});
+
+document.querySelectorAll('.right').forEach(function (element) {
+    element.addEventListener('click', right_Align);
+});
+
+document.querySelectorAll('.justify').forEach(function (element) {
+    element.addEventListener('click', Justify);
+});
+
